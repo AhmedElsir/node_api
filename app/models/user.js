0bfdaa41124227f49f4fa44f7b477baca87edcea
Ahmed ElSir <toto.90019@gmail.com>
2018-08-27 13:56:44 +0200

    start eslint

--- app/models/user.js
+++ app/models/user.js
@@ -1,15 +1,27 @@
-var mongoose     = require('mongoose');
+var mongoose        = require('mongoose');
 var uniqueValitator = require('mongoose-unique-validator');
-var Schema       = mongoose.Schema;
+var Schema          = mongoose.Schema;
+var crypto          = require(crypto);
 
 
 var UserSchema = new Schema({
     name: {type: String, required: true, unique: true},
     email: {type: String, required: true},
     phone_num : {type: String, required: true, unique: true},
-    token: {type: String, required: true, unique: true}
+    hash : String,
+    salt: String
 });
 
-UserSchema.plugin(uniqueValitator);
+UserSchema.methods.setPassword = function(password){
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10, 32, 'ahmed').toString('hex');
+}
 
+UserSchema.methods.validPassword = function(password){
+    var hash = crypto.pbkdf2Sync(password, this.salt, 10, 32, "ahmed").toString('hex');
+    return this.hash === hash;
+}
+
+
+UserSchema.plugin(uniqueValitator);
 module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -2,25 +2,38 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
-var jwt        = require('jwt-simple');
 
-//connect with mongoDB
+
+// call express
+const express = require('express');
+
+// define our app using express
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+
+const jwt = require('jwt-simple');
+
+const path = require('path');
+
+// connect with mongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/userdb', { useNewUrlParser: true });
 
-//check our connection
-var connection = mongoose.connection;
-connection.on('connected', function(){
-    console.log("connected to the db");
+// check our connection
+const { connection } = mongoose;
+connection.on('connected', () => {
+  console.log('connected to the db');
 });
 
+// set the view to views directory and set the view engine to be ejs
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-//pull our schema from user.js
-var User = require("./app/models/user");
 
+// pull our schema from user.js
+const User = require('./app/models/user');
 
 
 // configure app to use bodyParser()
@@ -28,121 +41,119 @@ var User = require("./app/models/user");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+const port = process.env.PORT || 8080; // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+const router = express.Router(); // get an instance of the express Router
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
-//middleware to use for all requests
-router.use(function(req, res, next){
-    //do logging
+// middleware to use for all requests
+router.use((req, res, next) => {
+  // do logging
 
-    console.log("something happens");
-    next();
+  console.log('something happens');
+  next();
 });
 
 router.route('/create')
 
-    .post(function(req, res){
-        //create a user (accessed at POST http://localhost:8080/user/create)
-        var user = new User;
-        user.name = req.body.name;
-        user.email = req.body.email;
-        user.phone_num = req.body.mobile;
-        let userToken = jwt.encode(user.name, req.body.password);
-        user.token = userToken;
-        
-        user.save(function(err){
-            if(err)
-                res.send((err));
-            
-            res.json({message: "user created"});
-        });
-    })
+  .post((req, res) => {
+    // create a user (accessed at POST http://localhost:8080/user/create)
+    const user = new User();
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.phone_num = req.body.mobile;
+    const userToken = jwt.encode(user.name, req.body.password);
+    user.token = userToken;
 
-    .get(function(req, res){
-        res.sendFile(__dirname + "/public/create.html")
-    })
+    user.save((err) => {
+      if (err) { res.send((err)); }
 
-
-//read all the user on the database
-router.route('/read')
-    .get(function(req, res){
-        User.find({}, ['name', '__id'],function(err, users){
-            if(err)
-                res.send(err);
-            //display only the user name and the id
-            res.json(users);
-        })
+      res.render('user_create', {
+        title: 'Bravo',
+      });
     });
+  })
+
+  .get((req, res) => {
+    res.render('create', { title: 'Create' });
+  });
+
+
+// read all the user on the database
+router.route('/read')
+  .get((req, res) => {
+    User.find({}, ['name', '__id', 'phone_num', 'email'], (err, users) => {
+      if (err) { res.send(err); }
+      // display only the user name and the id
+
+      res.render('read', {
+        title: 'All Users',
+        users,
+      });
+    });
+  });
 
 
 router.route('/read/:user_id')
-    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
-    .get(function(req, res){
-        User.findById(req.params.user_id, function(err, user){
-            if(err)
-                res.send(err);
-            
-            res.json(user);
-        })
+// get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+  .get((req, res) => {
+    User.findById(req.params.user_id, (err, user) => {
+      if (err) { res.send(err); }
+
+      decode_pass = jwt.decode(user.token, user.name);
+      res.render('user_id', {
+        title: user.name,
+        user,
+        pass: decode_pass,
+      });
     });
+  });
 
 
 router.route('/update/:user_id')
 
-    .get(function(req, res){
-        res.sendFile(__dirname + "/public/update.html");
-    })
+  .get((req, res) => {
+    res.sendFile(`${__dirname}/public/update.html`);
+  });
 
-//authenticate the user before it can update
+// authenticate the user before it can update
 router.route('/authenticate')
-    .post(function(req, res){
-        //find a user by it's token not an id
-        genToken = jwt.encode(req.body.old_name, req.body.old_password);
-        User.findOne({token: genToken}, function(err, user){
-            if(err)
-                res.send(err);
-            
-            //update the user information after we check it
-            if(req.body.new_name !== "")
-                user.name = req.body.new_name;
+  .post((req, res) => {
+    // find a user by it's token not an id
+    genToken = jwt.encode(req.body.old_name, req.body.old_password);
+    User.findOne({ token: genToken }, (err, user) => {
+      if (err) { res.send(err); }
 
-            if(req.body.new_email !== "")
-                user.email = req.body.new_email;
+      // update the user information after we check it
+      if (req.body.new_name !== '') { user.name = req.body.new_name; }
 
-            if(req.body.new_mobile !== "")
-                user.mobile = req.body.new_mobile;
-            
-            if(req.body.new_password !== "")
-            {
-                //if we use a new name and password gnerate a new token
-                if(req.body.new_name !== "")
-                {
-                    user.token = jwt.encode(req.body.new_name, req.body.new_password);
-                }
-                else
-                {
-                    user.token = jwt.encode(req.body.old_name, req.body.new_password);
-                }
-            }
+      if (req.body.new_email !== '') { user.email = req.body.new_email; }
 
-            user.save(function(err){
-                if(err) throw err;
-            });
-            res.json({message: "User succesfuly updated"});
-                
-        });
-    })
-    
+      if (req.body.new_mobile !== '') { user.mobile = req.body.new_mobile; }
+
+      if (req.body.new_password !== '') {
+        // if we use a new name and password gnerate a new token
+        if (req.body.new_name !== '') {
+          user.token = jwt.encode(req.body.new_name, req.body.new_password);
+        } else {
+          user.token = jwt.encode(req.body.old_name, req.body.new_password);
+        }
+      }
+
+      user.save((err) => {
+        if (err) throw err;
+      });
+      res.json({ message: 'User succesfuly updated' });
+    });
+  });
 
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+router.get('/', (req, res) => {
+  res.render('index');
 });
 
 // more routes for our API will happen here
@@ -154,4 +165,4 @@ app.use('/users', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log(`Magic happens on port ${port}`);
